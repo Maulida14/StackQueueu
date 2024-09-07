9f7a4b2ccc690b35d2f6f36ca3068b670c48bb3a
Oriana Nadienta Kadarsih <167696647+orianadienta@users.noreply.github.com>
2024-09-07 15:23:52 +0800

    Update Main.java

--- StackQueue/src/Main.java
+++ StackQueue/src/Main.java
@@ -3,6 +3,10 @@ public class Main {
         Stack<Integer> stackInt = new Stack<Integer>();
         Stack<Character> stackChar = new Stack<Character>();
 
+        Queue<Integer> integerQueue = new Queue<>();
+        Queue<Character> characterQueue = new Queue<>();
+
+        //buat nambahin value di stack
         stackInt.push(10);
         stackInt.push(20);
         stackInt.push(30);
@@ -12,9 +16,20 @@ public class Main {
         stackChar.push('a');
         stackChar.push('b');
 
-        stackInt.pop();
+        stackInt.pop(); //buat hapus value di stack
 
-        //stackInt.swap(1,2);
-        stackInt.hasPop();
+        stackInt.swap(1,2); //manggil swap buat nukar posisi value sesuai index
+        stackInt.hasPop(); //buat nampilin isi dari stack apabila terdapat value dan menampilkan keterangan false jika stack kosong
+
+        //buat tambahin value dengan tipe data char di queue
+        characterQueue.push('a');
+        characterQueue.push('b');
+        characterQueue.push('c');
+        characterQueue.push('d');
+        characterQueue.push('e');
+
+        characterQueue.swap(0,2); //swap value di queue sesuai index
+
+        
     }
 }
