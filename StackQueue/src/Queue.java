9df72f10e715fc7c890a3cb09feb078b7703deb5
Oriana Nadienta Kadarsih <167696647+orianadienta@users.noreply.github.com>
2024-09-07 15:26:26 +0800

    Update Queue.java

--- StackQueue/src/Queue.java
+++ StackQueue/src/Queue.java
@@ -1,4 +1,48 @@
-public class Queue {
-    public Node prev;
+public class Queue<T> {
+    private NodeQue<T> front;
+    private NodeQue<T> tail;
+
+    public Queue() {
+        this.front = null;
+        this.tail = null;    
+    }
+
+    public void swap(int index1, int index2) {
+        if (index1 < 0 || index2 < 0) {
+            System.out.println("Index tidak boleh negatif");
+            return;
+        }
+
+        if (index1 == index2) {
+            System.out.println("Index sama, tidak bisa menukar");
+            return;
+        }
+
+        NodeQue<T> node1 = null, node2 = null, current = front;
+        int i = 0;
+
+        // Cari node pada index1 dan index2
+        while (current != null) {
+            if (i == index1) {
+                node1 = current;
+            }
+            if (i == index2) {
+                node2 = current;
+            }
+            current = (NodeQue<T>) current.getNext();
+            i++;
+        }
+
+        // Jika salah satu node tidak ditemukan
+        if (node1 == null || node2 == null) {
+            System.out.println("Index melebihi panjang queue, terdapat node yang kosong");
+            return;
+        }
+
+        // Tukar nilai node1 dan node2
+        T temp = node1.getValue();
+        node1.setValue(node2.getValue());
+        node2.setValue(temp);
+    }
 
 }
